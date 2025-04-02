57a9791aade3b368422148c2a838b2111b5b1fa5
Adebayo Favour <fadebayo637@gmail.com>
2025-04-02 22:49:30 +0100

    Changes

--- src/components/Learning Dashboard/Learning_Dashboard_Comp.jsx
+++ src/components/Learning Dashboard/Learning_Dashboard_Comp.jsx
@@ -197,7 +197,7 @@ function Learning_Dashboard_Comp() {
                   onClick={() => handleTabClick(topic.id)}
                   className={`flex-1  flex items-center gap-3 p-3 rounded-lg mb-2 transition-colors ${
                     activeTab === topic.id
-                      ? 'bg-blue-500 text-white'
+                      ? 'bg-[#CD950E] text-white'
                       : isLocked
                       ? 'bg-gray-100 text-gray-400 cursor-not-allowed'
                       : 'hover:bg-gray-100 text-gray-700'
@@ -220,7 +220,7 @@ function Learning_Dashboard_Comp() {
                   onClick={() => handleTabClick(topic.id)}
                   className={`w-full flex items-center gap-3 p-3 rounded-lg mb-2 transition-colors ${
                     activeTab === topic.id
-                      ? 'bg-blue-500 text-white'
+                      ? 'bg-[#CD950E] text-white'
                       : isLocked
                       ? 'bg-gray-100 text-gray-400 cursor-not-allowed'
                       : 'hover:bg-gray-100 text-gray-700'
@@ -245,7 +245,7 @@ function Learning_Dashboard_Comp() {
               {topics.find(t => t.id === activeTab)?.content}
               <button
                 onClick={markAsCompleted}
-                className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition-colors"
+                className="px-4 py-2 bg-blue-500 text-white rounded-sm hover:bg-green-600 transition-colors"
               >
                 Mark as Completed
               </button>
